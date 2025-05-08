f728d8eab0f3761d7f24e761704602178e18f3d2
Daniel Reisman <daniel.reisman@student.fairfield.edu>
2025-05-08 09:28:28 -0400

    Pushed lib folder

---
+++ libs/functions/functions.cs
@@ -0,0 +1,9 @@
+
+private enum Functions {
+    Help, // Help Information
+    Report, // Run CMPivot Reports
+    Device, // Get Single Device Info
+    Collections, //Get Device Collection Info
+    Echo, // Echo/Print to the console
+    Exit // Exit from the console
+}
